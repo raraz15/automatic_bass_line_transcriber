df814b7340317d4ea6cb1a4bbedb399f6796bfb6
raraz15 <raraz15@ku.edu.tr>
2021-07-05 19:42:58 +0300

    dataset.py functional

--- dataset.py
+++ dataset.py
@@ -1,24 +1,86 @@
+#!/usr/bin/env python
+# coding: utf-8
+
 import os, sys
 import traceback
 from collections import Counter
 
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 
 import utilities
 import bassline_transcriber.transcription as transcription
-from representation import transpose_to_C
+from representation import transpose_to_C, encode_midi_sequence
 
-#ORIGINAL_MAX = 48 # C3 (middle C = C4 during transcription)
-#ORIGINAL_MIN = 23 # B1 
-
-#SILENCE_CODE = 0
-#SUSTAIN_CODE = 100
+# ORIGINAL_MAX = 48 # C3 (middle C = C4 during transcription)
+# ORIGINAL_MIN = 23 # B1
 
 # --------------------------------------- Dataframe Creation ---------------------------------------------------
 
-def create_midi_sequences_array(track_titles, track_dicts, directories, silence_code=0):
+def create_datasets(df, excluded_titles, track_dicts,  M, N_qb=8, silence_code=0, sustain_code=100, MIN_NOTE=28, MAX_NOTE=51):
+    codes, titles, keys, scales = [], [], [], []
+    for title, key, scale, midi_sequence in zip(df['Title'], df['Key'], df['Scale'], df.iloc[:, 3:].to_numpy()):   
+        try:  
+            if title not in excluded_titles:
+                key = track_dicts[title]['Key'].split(' ')[0]
+                code = encode_midi_sequence(midi_sequence, key, M, N_qb,
+                                            silence_code=silence_code, sustain_code=sustain_code,
+                                            MIN_NOTE=MIN_NOTE, MAX_NOTE=MAX_NOTE) 
+                if code is not None:
+                    codes.append(code)
+                    titles.append(title)
+                    keys.append(key)
+                    scales.append(scale)
+        except KeyboardInterrupt:
+            sys.exit(0)  
+        except FileNotFoundError:
+            pass
+        except Exception as ex:
+            print(''.join(traceback.format_exception(etype=type(ex), value=ex, tb=ex.__traceback__)))
+    codes = np.array(codes).reshape(-1,512//M)
+    df_codes = make_dataframe(codes, titles, keys, scales)
+    df_codes_min = df_codes[df_codes['Scale'] == "min"]
+    df_codes_maj = df_codes[df_codes['Scale'] == "maj"]
+    df_codes_min.reset_index(drop=True, inplace=True)
+    df_codes_maj.reset_index(drop=True, inplace=True)
+    print('Number of total representations: {}\n'.format(df_codes.shape[0]))
+    print('Tokens:\n{}\n'.format(np.unique(df_codes.iloc[:, 3:].to_numpy())))
+    print(df_codes['Scale'].value_counts())
+    return df_codes_min, df_codes_maj
+
+def make_dataframe(X, titles, keys, scales):
+    df = pd.DataFrame(X)
+    df['Title'] = titles
+    df['Key'] = keys
+    df['Scale'] = scales
+    df = df.reindex(columns=['Title','Key','Scale']+[x for x in np.arange(X.shape[1])])
+    return df
+
+def create_single_octave_dataset(df, C, silence_code=0, sustain_code=25):
+    data = df.iloc[:, 3:]
+    if sustain_code is not None:
+        data[data==sustain_code] = 1000    
+    for i in reversed(range(1, 4)): # fold the higher octaves
+        data[data>=C+i*12] -= 12
+    for i in reversed(range(0, 4)): # fold the lower octaves
+        data[(data!=silence_code) & (data < C-i*12)] += 12
+    data[data!=silence_code] -= C-1 # shift to 1
+    data[data>900] = 13 
+    df_single = pd.concat([df.iloc[:,:3], data], axis=1)      
+    return df_single
+
+
+def create_groove_dataset(df, silence_code=0, sustain_code=25):
+    """Create groove dataset from encoded dataset. SUS none for no sustain"""
+    df_groove = df.iloc[:,3:].copy()
+    df_groove[(df_groove != silence_code) & (df_groove != sustain_code)] = 1 # convert all notes to "on"
+    df_groove[df_groove == sustain_code] = 2 
+    df_groove = pd.concat([df.iloc[:,:3], df_groove], axis=1) # bring back the metadata
+    print('Number of total representations: {}\n'.format(df_groove.shape[0]))
+    print('Tokens:\n{}\n'.format(np.unique(df_groove.iloc[:, 3:].to_numpy())))
+    return df_groove
+
+def create_midi_sequences_dataframe(track_titles, track_dicts, directories, silence_code=0):
     midi_sequences, valid_titles, keys, scales = [], [], [], []
     for title in track_titles:
         try:
@@ -37,46 +99,13 @@ def create_midi_sequences_array(track_titles, track_dicts, directories, silence_
             print(''.join(traceback.format_exception(etype=type(ex), value=ex, tb=ex.__traceback__)))
     midi_sequences = np.array(midi_sequences).reshape(-1, 512)
     print('There are {} midi sequences.'.format(midi_sequences.shape[0]))
-    return midi_sequences, valid_titles, keys, scales
-
-
-def load_data(data_params):     
-    dataset_path, scale_type, M = data_params['dataset_path'], data_params['scale_type'], data_params['M']    
-    dataset_name = data_params['dataset_name'] +'_{}_M{}.csv'.format(scale_type, M)
-    dataset_dir = os.path.join(dataset_path, dataset_name)
-    df = pd.read_csv(dataset_dir, header=None)
-    titles = df[0].tolist()
-    X = df[df.columns[1:]].to_numpy()    
-    return X, titles
-
-def append_SOS(X, SOS_token=-1):
-    X = np.concatenate( (SOS_token*np.ones((X.shape[0],1), dtype=np.int64), X), axis=1)    
-    return X+1 
-
-def make_dataframe(X, titles, keys, scales):
-    df = pd.DataFrame(X)
-    df['Title'] = titles
-    df['Key'] = keys
-    df['Scale'] = scales
-    df = df.reindex(columns=['Title']+['Key']+['Scale']+[x for x in np.arange(X.shape[1])])
+    df = make_dataframe(midi_sequences, valid_titles, keys, scales)
     return df
 
-
-def bars_to_representation(bar, M, N_bars, key):
-        
-    midi_array = transcription.frequency_to_midi_array(bar, M, N_bars, silence_code=0)
-      
+def bars_to_representation(bar, M, N_bars, key):       
+    midi_array = transcription.frequency_to_midi_array(bar, M, N_bars, silence_code=0)  
     representation = transcription.encode_midi_array(midi_array, M, N_bars, key, silence_code=0, sustain_code=100)
-    
     return representation
-
-
-def merge_track_dicts(*args):
-    track_dicts = {}
-    for dct in args:
-        track_dicts = {**track_dicts, **dct}
-    return track_dicts
-
 
 # TODO: MAKE THEM WORK
 # --------------------- Augmentation --------------------------
@@ -130,7 +159,6 @@ def repeat_dataset(df_titles, track_dicts, N_bars_repeat, directories, M):
     print('Before concat: {}'.format(representations.shape))
     representations = np.concatenate([representations]*(N_bars//N_bars_repeat), axis=1)
     print('After concat: {}'.format(representations.shape))
-    
     return representations, bar_titles
 
 # ------------------------- Analysis ---------------------------------------
@@ -146,7 +174,7 @@ def count_keys(df, track_dicts):
 def count_notes(df):
     X = df.iloc[:, 3:].to_numpy()
     note_counter= Counter()
-    for i, midi_sequence in enumerate(X):
+    for _, midi_sequence in enumerate(X):
         try:    
             for note in midi_sequence:
                 note_counter[note] += 1
@@ -156,7 +184,6 @@ def count_notes(df):
             pass
         except Exception as ex:
             print(''.join(traceback.format_exception(etype=type(ex), value=ex, tb=ex.__traceback__)))
-    #del note_counter[0]
     note_counter = dict(sorted(note_counter.items(), key=lambda x: x[0]))
     return note_counter
 
@@ -182,22 +209,15 @@ def count_notes_with_transposing(df):
     note_counter_T = dict(sorted(note_counter_T.items(), key=lambda x: x[0]))       
     return note_counter, note_counter_T
 
-def count_same_phrases(code, M, counter):
-    
+def count_same_phrases(code, M, counter):  
     code_re = code.reshape((4,4, 4*(8//M)))
-    
     for b in range(4):
-
         bars = code_re[:,b,:] # beat b for all bars
-
         for i in range(4):
             for j in range(i+1,4):
-
                 B0 = bars[i,:]
                 B1 = bars[j,:]
-
                 if np.array_equal(B0, B1):
                     key = '{}{}'.format(i,j)
                     counter[b][key] += 1
-                    
     return counter 
