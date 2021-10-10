396223d603224f23dd0f241a34f8f69d9b70ce12
oguz_araz <raraz15@ku.edu.tr>
2021-10-10 17:16:57 +0200

    figures dir

--- ablt/directories.py
+++ ablt/directories.py
@@ -10,6 +10,7 @@ DATA_DIR = os.path.join(ROOT_DIR, 'data')
 METADATA_DIR = os.path.join(DATA_DIR, 'metadata')
 AUDIO_DIR = os.path.join(DATA_DIR, 'audio_clips')
 OUTPUT_DIR = os.path.join(DATA_DIR, 'outputs')
+FIGURES_DIR = os.path.join(DATA_DIR, 'figures')
 
 TRACK_DICTS_PATH = os.path.join(METADATA_DIR, "track_dicts.json")
 SCALE_FREQUENCIES_PATH = os.path.join(METADATA_DIR, "scales_frequencies.json")
