cbb05aec4559bf08c79082b00782bc75ba4933ca
oguz_araz <raraz15@ku.edu.tr>
2021-10-11 22:35:28 +0200

    improved plotting

--- ablt/plotting/building_blocks.py
+++ ablt/plotting/building_blocks.py
@@ -43,20 +43,18 @@ def form_beat_grid_waveform(beat_positions, audio_array, fs, ax):
 
     librosa.display.waveplot(audio_array, sr=fs, ax=ax)
     
-    ax.vlines(bar_positions, -1.1, 1.1, alpha=0.8, color='g',linestyle='dashed', linewidths=3)
-    ax.vlines(beat_positions_plotting, -0.9, 0.9, alpha=0.8, color='r',linestyle='dashed', linewidths=3)
-    ax.vlines(quarter_beat_positions, -0.7, 0.7, alpha=0.8, color='k',linestyle='dashed', linewidths=3)
+    ax.vlines(bar_positions, -1.1, 1.1, alpha=0.8, color='g',linestyle='dashed', linewidths=3, label='Bar Positions')
+    ax.vlines(beat_positions_plotting, -0.9, 0.9, alpha=0.8, color='r',linestyle='dashed', linewidths=3, label='Beat Positions')
+    ax.vlines(quarter_beat_positions, -0.7, 0.7, alpha=0.8, color='k',linestyle='dashed', linewidths=3, label='Quarter Beat Positions')
     
     ax.set_xlim([-0.05, (len(audio_array)/fs)+0.05])
-    
-    ax.set_ylabel('Amplitude')        
-    ax.xaxis.label.set_size(18)
-    ax.yaxis.label.set_size(18)
+
+    ax.set_title('Waveform', fontsize=20)
+    ax.set_ylabel('Amplitude', fontsize=18)
+    ax.set_xlabel('Time(s)', fontsize=18)      
 
     ax.tick_params(axis='both', which='major', labelsize=14)
     
-    ax.set_title('Waveform', fontsize=15)
-
 def form_beat_grid_spectrogram(beat_positions, spectrogram, fs, hop_length, ax):
     """
     Plots the bar, beat and quarter beats on a given spectrogram plt.ax
@@ -66,21 +64,20 @@ def form_beat_grid_spectrogram(beat_positions, spectrogram, fs, hop_length, ax):
 
     librosa.display.specshow(spectrogram, sr=fs, hop_length=hop_length, x_axis='time', y_axis='log', ax=ax)
 
-    ax.vlines(bar_positions, 0, 512, alpha=0.8, color='g',linestyle='dashed', linewidths=3)
-    ax.vlines(beat_positions_plotting, 0, 256, alpha=0.8, color='r',linestyle='dashed', linewidths=3)
-    ax.vlines(quarter_beat_positions, 0, 170, alpha=0.8, color='k',linestyle='dashed', linewidths=3)
+    ax.vlines(bar_positions, 0, 512, alpha=0.8, color='g',linestyle='dashed', linewidths=3, label='Bar Positions')
+    ax.vlines(beat_positions_plotting, 0, 256, alpha=0.8, color='r',linestyle='dashed', linewidths=3, label='Beat Positions')
+    ax.vlines(quarter_beat_positions, 0, 170, alpha=0.8, color='k',linestyle='dashed', linewidths=3, label='Quarter Beat Positions')
     
     ax.set_xlim([-0.05, (spectrogram.shape[1]*hop_length/fs)+0.05])
     ax.set_ylim([-5,512]) 
 
-    ax.yaxis.label.set_size(14)
-    ax.xaxis.label.set_size(14)    
+    ax.set_title('Spectrogram', fontsize=20, loc="center")
+    ax.set_xlabel('Time(s)', fontsize=18)
+    ax.set_ylabel('Hz', fontsize=18)    
 
     ax.yaxis.set_ticks(np.array([0,32,48,64,96,128,256,512]))
     ax.tick_params(axis='both', which='major', labelsize=14)
 
-    ax.set_title('Spectrogram', fontsize=15)
- 
 def form_pitch_track(F0_estimate, ax, color='b', label=''):
     """
     Plots the F0_estimate on a given plt.ax
@@ -91,6 +88,10 @@ def form_pitch_track(F0_estimate, ax, color='b', label=''):
     markerline.set_markerfacecolor(color)
     markerline.set_markersize(9)
     stemlines.set_linewidth(0)
+
+def create_sup_title(fig, title):
+    mid = (fig.subplotpars.right + fig.subplotpars.left)/2
+    fig.suptitle(title, fontsize=22, x=mid)    
 
 # DEPRACATED
 def form_notes(ax, notes, unk_notes):
